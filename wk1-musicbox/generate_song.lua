b5b9702484a1c31c930a83fbf37e4e83de659fc7
Leonard Truong <lennytruong@berkeley.edu>
2015-01-28 18:41:12 -0800

    Generate song complete

--- wk1-musicbox/generate_song.lua
+++ wk1-musicbox/generate_song.lua
@@ -1,3 +1,15 @@
+require("math")
+math.randomseed(os.time())
+
 function generate_song(length)
-   return 1
+   local song = {}
+   for i=1,length do
+      song[i] = math.random(3)
+   end
+   return song
+end
+
+song = generate_song(3)
+for i=1,3 do
+   print(song[i])
 end
