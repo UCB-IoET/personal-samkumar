2284642756abdedcff498ae9ceeca3bc21956745
Sam Kumar <samkumar@berkeley.edu>
2015-01-30 00:00:21 -0800

    Switched to using cords to reduce memory usage. Also tweakedtimes and frequencies

--- wk1-musicbox/song.lua
+++ wk1-musicbox/song.lua
@@ -1,85 +1,45 @@
 shield = require "starter"
--- cord = require "cord"
 math = require "math"
 
 -- seed with current time
 math.randomseed(storm.os.now(storm.os.SHIFT_0))
+
 shield.LED.start()
 shield.Buzz.start()
 
 function generate(length)
    local song = {}
-   for i=1,length do
+   for i = 1, length do
       song[i] = math.random(3)
    end
    return song
 end
 
--- song = generate(3)
-
--- for i=1,3 do
---    print(song[i])
--- end
-
-function buzz(note)
-   shield.Buzz.go(note)
-   storm.os.invokeLater(10 * storm.os.MILLISECOND, function() shield.Buzz.stop() end)
-end
-
--- function buzz_length(note, length)
---    Buzz.go(note)
---    cord.await(storm.os.invokeLater, length * storm.os.millisecond)
---    Buzz.stop()
--- end
-
 local note_to_color = {[1]="blue", [2]="green", [3]="red"}
-
--- function flash_length(note, length)
---    Led.flash(note_to_color[note], length)
--- end
-
--- Wrapper around 
-function flash(note)
-   shield.LED.flash(note_to_color[note], 10)
-end
+local note_to_period = {[1] = 1 * storm.os.MILLISECOND, [2] = 3 * storm.os.MILLISECOND, [3] = 7 * storm.os.MILLISECOND}
 
 -- Plays a note by buzzing and flashing
 function play_note(note)
-   buzz(note)
-   flash(note)
+   shield.Buzz.go(note_to_period[note])
+   shield.LED.on(note_to_color[note])
+   cord.await(storm.os.invokeLater, 500 * storm.os.MILLISECOND)
+   shield.Buzz.stop()
+   shield.LED.off(note_to_color[note])
 end
 
--- function play_note_length(note, length)
---    flash_length(note, length)
---    buzz_length(note, length)
--- end
-
-function play(s, index)
-   index = index or 1
-   play_note(s[index])
-   if index < #s then
-      storm.os.invokeLater(50 * storm.os.MILLISECOND, function() play(s, index + 1) end)
+function play(s)
+   for i = 1, #s do
+      play_note(s[i])
+      cord.await(storm.os.invokeLater, 400 * storm.os.MILLISECOND)
    end
 end
 
--- function play_song_var_length(s, length_arr, index)
---    index = index or 1
---    play_note_length(s[index])
---    if index < #s then
---       storm.os.invokeLater(50 * storm.os.MILLISECOND, function() play_song_var_length(s, length_arr, index + 1) end)
---    end
--- end
+cord.new(function ()
+    while true do
+       local s = generate(5)
+       play(s)
+       cord.await(storm.os.invokeLater, storm.os.SECOND)
+    end
+end)
 
---------------------------------------------
--- local song = {}
-
--- song.flash = flash
--- song.play_note = play_note
--- song.generate = generate
--- song.play = play
--- return song
-
-while true do
-   local s = generate(5)
-   play(s)
-end
+cord.enter_loop()
