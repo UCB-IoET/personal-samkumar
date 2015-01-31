e1192df668df73c4a73ac442a65b9e7e71dbf460
Sam Kumar <samkumar@berkeley.edu>
2015-01-31 00:33:43 -0800

    Made subsequent games start consistently, but randomly freezes in the middle of notes

--- wk1-musicbox/mainloop.lua
+++ wk1-musicbox/mainloop.lua
@@ -1,6 +1,46 @@
 shield = require "starter"
-song = require "song"
 buttons = {[1] = "right", [2] = "middle", [3] = "left"}
+
+function setup()
+    -- seed with current time
+    math.randomseed(storm.os.now(storm.os.SHIFT_0))
+
+    shield.LED.start()
+    shield.Buzz.start()
+    shield.Button.start()
+end
+
+function generate(len, song)
+   local i = 1
+   for i = 1, len do
+      song[i] = math.random(3)
+   end
+end
+
+local note_to_color = {[1]="blue", [2]="green", [3]="red"}
+local note_to_period = {[1] = 1 * storm.os.MILLISECOND, [2] = 3 * storm.os.MILLISECOND, [3] = 7 * storm.os.MILLISECOND}
+
+-- Plays a note by buzzing and flashing
+function play_note(note)
+   print("playing note")
+   shield.Buzz.go(note_to_period[note])
+   shield.LED.on(note_to_color[note])
+   print("waiting")
+   cord.await(storm.os.invokeLater, 500 * storm.os.MILLISECOND)
+   print("finished waiting")
+   shield.Buzz.stop()
+   shield.LED.off(note_to_color[note])
+end
+
+function play(s)
+   local i = 1
+   for i = 1, #s do
+      print("about to play note")
+      play_note(s[i])
+      cord.await(storm.os.invokeLater, 400 * storm.os.MILLISECOND)
+   end
+end
+
 local mysong = {}
 local next_note = 1 -- the index of the note we expect the user to press next
 local correct = true
@@ -10,7 +50,7 @@ local eval_press = function (note)
     local expected_note = mysong[next_note]
     if note == expected_note then
         cord.new(function() 
-          song.play_note(note)
+          play_note(note)
         end)
         next_note = next_note + 1
         print(mysong[next_note])
@@ -31,8 +71,16 @@ local eval_press = function (note)
 end
 
 function playmusicbox(length)
-    song.generate(length, mysong)
-    song.play(mysong)
+    generate(length, mysong)
+    local i = 1
+    for i = 1, #mysong do
+        shield.Buzz.go(note_to_period[mysong[i]])
+       shield.LED.on(note_to_color[mysong[i]])
+       cord.await(storm.os.invokeLater, 500 * storm.os.MILLISECOND)
+       shield.Buzz.stop()
+       shield.LED.off(note_to_color[mysong[i]])
+        cord.await(storm.os.invokeLater, 400 * storm.os.MILLISECOND)
+    end
     next_note = 1
     correct = true
     print(mysong[1])
@@ -45,8 +93,8 @@ for i = 1, 3 do
     end)
 end
 
-song.setup()
-song.setup = nil
+setup()
+setup = nil
 shield.Buzz.start = nil
 shield.LED.start = nil
 shield.Button.start = nil
