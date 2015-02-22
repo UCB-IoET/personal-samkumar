0949e5ee80800db3006b147fd5ed27b7e914030e
Michael Ho <callmemikeh@gmail.com>
2015-02-21 17:05:52 -0800

    allarm updated with display and time code
    real time clock not implemented yet. untested, moving to testing

--- wk3-allarm/allarm.lua
+++ wk3-allarm/allarm.lua
@@ -24,18 +24,18 @@ starter.button.whenever(1, "RISING", function()
 	if mode == 0 then
 		printServices()
 	elseif mode == 1 then
-		mode = 0
+		changeMode(0)
 		time_until = alarm_time - (hour * 60 + min)
 		cur_alarm = storm.os.invokeLater(time_until * storm.os.MINUTE, function() allarm() end)
 	else
-		mode = 0
+		changeMode(0)
 		allunarm()
 	end
 end)
 starter.button.whenever(2, "FALLING", function() 
 	if mode == 0 then
 		if starter.Button.pressed("D11") then
-			mode = 1
+			changeMode(1)
 			alarm_time = hour * 60 + min;
 		end
 	else
@@ -45,13 +45,22 @@ end)
 starter.button.whenever(3, "FALLING", function() 
 	if mode == 0 then
 		if starter.Button.pressed("D10") then
-			mode = 1
+			changeMode(1)
 			alarm_time = hour * 60 + min;
 		end
 	else
 		alarm_time = alarm_time + 1
 	end
 end)
+
+function changeMode(m)
+	mode = m
+	if mode == 2 then
+		d:num(8888)
+	else
+		show_time()
+	end
+end
 
 function printServices()
 	--print info about discovered services
@@ -67,17 +76,19 @@ end
 
 --send alarm signal!
 function allarm()
-	mode = 2
+	changeMode(2)
 	led_services = discoverer.resolve("setLED")
 	buzz_services = discoverer.resolve("setBuzzer")
 	send_all(1)
+	d:num(8888)
 end
 
 function allunarm()
-	mode = 0
+	changeMode(0)
 	send_all(0)
 	led_services = nil
 	buzz_services = nil
+	show_time()
 end	
 
 function send_all(arg)
@@ -92,6 +103,18 @@ function show_time()
 	d:time(hour, min)
 end
 
-storm.os.invokePeriodically(storm.os.MINUTE, function() if mode == 0 then show_time() end end)
+--start clock running
+hour = 0 --replace these three declarations with the first time upon initialization
+min = 0
+sec = 0
+show_time()
+storm.os.invokeLater((60 - sec) * storm.os.SECOND, function()
+	storm.os.invokePeriodically(storm.os.MINUTE, function()
+		min = min + 1
+		if mode == 0 then
+			show_time()
+		end
+	end)
+end)
 
 cord.enter_loop()
