31fccac0624e34ffa1af00cd415b320168c1bb2a
Leonard Truong <leonardtruong@gmail.com>
2015-04-15 17:42:20 -0700

    Removing tabs.

--- final-chair/chairsettings.lua
+++ final-chair/chairsettings.lua
@@ -22,24 +22,24 @@ for _, heater in pairs(heaters) do
    (function (heater)
        cord.new(function ()
 		   local setting = nil
-		   while true do
-		      setting = 10 * heaterSettings[heater] * storm.os.MILLISECOND
+       while true do
+          setting = 10 * heaterSettings[heater] * storm.os.MILLISECOND
           if not storm.n.check_occupancy() then
              setting = 0
           end
-		      if setting <= 0 then
+          if setting <= 0 then
              cord.yield()
-		      else
+          else
              storm.n.set_heater_state(heater, storm.n.ON)
              cord.await(storm.os.invokeLater, setting)
-		      end
-		      if setting >= storm.os.SECOND then
+          end
+          if setting >= storm.os.SECOND then
              cord.yield()
-		      else
+          else
              storm.n.set_heater_state(heater, storm.n.OFF)
              cord.await(storm.os.invokeLater, storm.os.SECOND - setting)
-		      end
-		   end
+          end
+       end
        end)
     end)(heater)
 end
