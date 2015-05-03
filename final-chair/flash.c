abf8fe421e1bb4d9dee061984c60d7aad0498f83
Sam Kumar <samkumar@berkeley.edu>
2015-05-03 14:46:15 -0700

    Create API for logging to flash

--- final-chair/flash.c
+++ final-chair/flash.c
@@ -8,39 +8,6 @@
 
 #define get_sp_bits(a) (a & (1 << 23))
 
-/* read_sp_index(index, cb)
-
-   Gets the stack pointer value at INDEX, and returns it. */
-/*int read_sp_index_async(lua_State* L) {
-    int index = luaL_checkint(L, 1);
-    lua_pushlightfunction(L, libstorm_flash_read);
-    lua_pushnumber(L, index << PAGE_EXP);
-    lua_pushvalue(L, 2);
-    storm_array_nc_create(L, 1, ARR_TYPE_INT32); // seriously, why isn't there an ARR_TYPE_UINT32?
-    lua_call(L, 2, 1);
-}
-
-int read_sp_index(lua_State* L) {
-    storm_array_nc_create(L, 1, ARR_TYPE_INT32); // seriously, why isn't there an ARR_TYPE_UINT32?
-    int arr_index = lua_gettop(L);
-    lua_pushvalue(L, arr_index);
-    cord_set_continuation(L, read_sp_index_tail, 1);
-    lua_getglobal(L, "cord");
-    lua_pushstring(L, "await");
-    lua_rawget(L, -2);
-    lua_remove(L, -2);
-    lua_pushlightfunction(L, libstorm_flash_read);
-    int index = luaL_checkint(L, 1);
-    lua_pushnumber(L, index << PAGE_EXP);
-    lua_pushvalue(L, arr_index);
-    return cord_invoke_custom(L, 4);
-}
-
-int read_sp_index_tail(lua_State* L) {
-    lua_pushvalue(L, lua_upvalueindex(1));
-    return cord_return(L, 1);
-}*/
-
 int call_fn(lua_State* L) {
     lua_pushvalue(L, lua_upvalueindex(1));
     lua_pushvalue(L, lua_upvalueindex(2));
@@ -185,6 +152,14 @@ int write_sp_3(lua_State* L) {
     return 0;
 }
 
+int reset_log(lua_State* L) {
+    lua_pushlightfunction(L, write_sp);
+    lua_pushnumber(L, 3 << PAGE_EXP);
+    lua_pushvalue(L, 1); // callback
+    lua_call(L, 2, 0);
+    return 0;
+}
+
 /** FORMAT OF A LOG ENTRY
     Each log entry has the following format:
     1. Timestamp (32 bits)
@@ -201,73 +176,157 @@ int write_sp_3(lua_State* L) {
     Total of 112 bits (14 bytes) per entry
 **/
 
-// write_log_entry(time_offset, backh, bottomh, backf, bottomf, temp, hum, occ)
+int write_log_entry_cb(lua_State* L);
+int write_log_entry_cleanup(lua_State* L);
+
+// write_log_entry(time_offset, backh, bottomh, backf, bottomf, temp, hum, occ, reboot, callback)
 int write_log_entry(lua_State* L) {
+    int i, arr_index;
+    uint8_t bytes[LOG_ENTRY_LEN];
+    
+    lua_pushlightfunction(L, libstorm_os_now);
+    lua_pushnumber(L, 2);
+    lua_call(L, 1, 1);
+    uint32_t timeshift1 = (uint32_t) lua_tointeger(L, -1);
+    lua_pop(L, 1);
+    lua_pushlightfunction(L, libstorm_os_now);
+    lua_pushnumber(L, 3);
+    lua_call(L, 1, 1);
+    uint32_t timeshift2 = (uint32_t) lua_tointeger(L, -1);
+    lua_pop(L, 1);
+    
+    // we can ignore timer_getnow(). It is 16 bits and milliseconds are 375 ticks, so we'll be off by at most 175 ms, which is OK.
+    uint64_t uppertime = (((uint64_t) timeshift2) << 32) | ((uint64_t) timeshift1);
+    uint64_t seconds = (uint64_t) (8192 * uppertime / 46875.0); // convert to seconds
+    uint64_t time = seconds + luaL_checkint(L, 1);
+    uint32_t timestamp = (uint32_t) time; // At this point I should have seconds since the epoch, so 32 bits wil be OK
+    uint8_t backh = (uint8_t) luaL_checkint(L, 2);
+    uint8_t bottomh = (uint8_t) luaL_checkint(L, 3);
+    uint8_t backf = (uint8_t) luaL_checkint(L, 4);
+    uint8_t bottomf = (uint8_t) luaL_checkint(L, 5);
+    uint16_t temperature = (uint16_t) luaL_checkint(L, 6);
+    uint16_t humidity = (uint16_t) luaL_checkint(L, 7);
+    luaL_checktype(L, 9, LUA_TBOOLEAN);
+    uint8_t secondlastbyte = (((uint8_t) luaL_checkint(L, 8)) << 7) | ((uint8_t) lua_toboolean(L, 9) << 6) | 0x20; // for now, the time is always known
+    
+    *((uint32_t*) bytes) = timestamp;
+    bytes[4] = backh;
+    bytes[5] = bottomh;
+    bytes[6] = backf;
+    bytes[7] = bottomf;
+    *((uint16_t*) (bytes + 8)) = temperature;
+    *((uint16_t*) (bytes + 10)) = humidity;
+    bytes[12] = secondlastbyte;
+    bytes[13] = 0; // extra space
+    
+    // create array to write
+    storm_array_nc_create(L, LOG_ENTRY_LEN, ARR_TYPE_UINT8);
+    arr_index = lua_gettop(L);
+    for (i = 0; i < LOG_ENTRY_LEN; i++) {
+        lua_pushlightfunction(L, arr_set);
+        lua_pushvalue(L, arr_index);
+        lua_pushnumber(L, i + 1);
+        lua_pushnumber(L, bytes[i]);
+        lua_call(L, 3, 0);
+    }
+    
+    // read the stack pointer, write, and then update it
+    lua_pushlightfunction(L, read_sp);
+    lua_pushvalue(L, arr_index);
+    lua_pushvalue(L, 10); // the callback
+    lua_pushcclosure(L, write_log_entry_cb, 2);
+    lua_call(L, 1, 0);
     return 0;
 }
 
-/*
-uint32_t get_sp(lua_State *L, uint32_t ptr)
-{
-	lua_pushnumber(L, 0);
-	storm_array_nc_create(L, 1, ARR_TYPE_UINT32);
-	lua_pushnumber(L, 0);
-	lua_pushlightfunction(L, libstorm_flash_read);
-	lua_call(L, 3, 0);
-	lua_pop(L, 1);
-	lua_pushnumber(L, 1);
-	lua_pushlightfunction(L, arr_get);
-	lua_call(L, 2, 1);
-	uint32_t addr = lua_tonumber(L, -1);
-	lua_pop(L, 1);
-	return addr;
-}
-
-uint32_t find_top_of_stack(lua_State *L)
-{
-	//returns pointer to top of stack
-	uint32_t addr = get_sp(0);
-	uint32_t valid = (addr >> 23) & 1;
-	if (valid) {
-		return get_sp_bits(addr);
-	} else {
-		addr = get_sp(SP_LEN);
-		valid = (addr >> 23) & 1;
-		if (valid) {
-			return get_sp_bits(addr);
-		}
-	}
-
-	//panic: should not get here!
-	//TODO: what to do if neither sp is valid?
-	return ERROR;
-}
-
-int lua_get_tos(lua_State *L)
-{
-	lua_pushnumber(L, find_top_of_stack(L));
-	return 1;
-}
-
-int write_top_of_stack(lua_State *L)
-{
-	flash_xfer_t *t;
-    int rv;
-    uint32_t addr = lua_tonumber(L, 1);
-    storm_array_t *txarr = lua_touserdata(L, 2);
-    t = malloc(sizeof(flash_xfer_t));
-    if (!t)
-    {
-        return luaL_error( L, "out of memory");
-    }
-    t->cb_ref = luaL_ref(L, LUA_REGISTRYINDEX);
-    t->buf_ref = luaL_ref(L, LUA_REGISTRYINDEX);
-    rv = flash_write(addr, ARR_START(txarr), txarr->len, flash_xfer_cb, t);
-    if (rv != 0)
-    {
-        free(t);
-        return luaL_error( L, "bad flash tx");
-    }
+int write_log_entry_cb(lua_State* L) {
+    lua_pushlightfunction(L, libstorm_flash_write);
+    lua_pushvalue(L, 1);
+    lua_pushvalue(L, lua_upvalueindex(1));
+    lua_pushvalue(L, 1);
+    lua_pushvalue(L, lua_upvalueindex(2));
+    lua_pushcclosure(L, write_log_entry_cleanup, 2);
+    lua_pushcclosure(L, delay_handler, 1);
+    lua_call(L, 3, 0);
     return 0;
 }
-*/
+
+int write_log_entry_cleanup(lua_State* L) {
+    uint32_t prev_sp = lua_tonumber(L, lua_upvalueindex(1));
+    uint32_t new_sp = prev_sp + LOG_ENTRY_LEN;
+    uint32_t next_page = ((new_sp >> PAGE_EXP) + 1) << PAGE_EXP;
+    if (next_page - new_sp < LOG_ENTRY_LEN) {
+        new_sp = next_page;
+    }
+    lua_pushlightfunction(L, write_sp);
+    lua_pushnumber(L, new_sp);
+    lua_pushvalue(L, lua_upvalueindex(2)); // the callback
+    lua_call(L, 2, 0);
+    return 0;
+}
+
+int read_log_entry_cb(lua_State* L);
+int read_log_entry(lua_State* L) {
+    int index = luaL_checkint(L, 1);
+    uint32_t entries_per_page = PAGE_SIZE / LOG_ENTRY_LEN;
+    uint32_t page = index / entries_per_page;
+    uint32_t page_offset = index % entries_per_page;
+    uint32_t flash_addr = LOG_START + (page << PAGE_EXP) + (page_offset * LOG_ENTRY_LEN);
+    
+    storm_array_nc_create(L, LOG_ENTRY_LEN, ARR_TYPE_UINT8);
+    int arr_index = lua_gettop(L);
+    
+    lua_pushlightfunction(L, libstorm_flash_read);
+    lua_pushnumber(L, flash_addr);
+    lua_pushvalue(L, arr_index);
+    lua_pushvalue(L, arr_index);
+    lua_pushvalue(L, 2); // the callback
+    lua_pushcclosure(L, read_log_entry_cb, 2);
+    lua_pushcclosure(L, delay_handler, 1);
+    lua_call(L, 3, 0);
+    return 0;
+}
+
+int read_log_entry_cb(lua_State* L) {
+    int i;
+    int arr_index = lua_upvalueindex(1);
+    uint8_t bytes[LOG_ENTRY_LEN];
+    for (i = 0; i < LOG_ENTRY_LEN; i++) {
+        lua_pushlightfunction(L, arr_get);
+        lua_pushvalue(L, arr_index);
+        lua_pushnumber(L, i + 1);
+        lua_call(L, 2, 1);
+        bytes[i] = lua_tointeger(L, -1);
+        lua_pop(L, 1);
+    }
+    
+    uint32_t timestamp = *((uint32_t*) bytes);
+    uint8_t backh = bytes[4];
+    uint8_t bottomh = bytes[5];
+    uint8_t backf = bytes[6];
+    uint8_t bottomf = bytes[7];
+    uint16_t temperature = *((uint16_t*) (bytes + 8));
+    uint16_t humidity = *((uint16_t*) (bytes + 10));
+    uint8_t secondlastbyte = bytes[12];
+    uint8_t occ = (secondlastbyte >> 7);
+    uint8_t rebooted = (secondlastbyte >> 6) & 0x1;
+    uint8_t known_time = (secondlastbyte >> 5) & 0x1;
+    
+    lua_pushvalue(L, lua_upvalueindex(2)); // the callback
+    if (known_time) {
+        lua_pushnumber(L, timestamp);
+    } else {
+        lua_pushnil(L);
+    }
+    lua_pushnumber(L, backh);
+    lua_pushnumber(L, bottomh);
+    lua_pushnumber(L, backf);
+    lua_pushnumber(L, bottomf);
+    lua_pushnumber(L, temperature);
+    lua_pushnumber(L, humidity);
+    lua_pushnumber(L, occ);
+    lua_pushnumber(L, rebooted);
+    lua_call(L, 9, 0);
+    
+    return 0;
+}
--- final-chair/flash.h
+++ final-chair/flash.h
@@ -1,6 +1,9 @@
 #define PAGE_SIZE 256
 #define PAGE_EXP 8
 
+#define LOG_ENTRY_LEN 14
+#define LOG_START 768
+
 int read_sp(lua_State* L);
 int write_sp(lua_State* L);
 
@@ -10,4 +13,7 @@ int libstorm_flash_write(lua_State* L);
 
 #define FLASH_SYMBOLS \
     { LSTRKEY("flash_read_sp"), LFUNCVAL(read_sp) }, \
-    { LSTRKEY("flash_write_sp"), LFUNCVAL(write_sp) },
+    { LSTRKEY("flash_write_sp"), LFUNCVAL(write_sp) }, \
+    { LSTRKEY("flash_reset_log"), LFUNCVAL(reset_log) }, \
+    { LSTRKEY("flash_write_log"), LFUNCVAL(write_log_entry) }, \
+    { LSTRKEY("flash_read_log"), LFUNCVAL(read_log_entry) },
