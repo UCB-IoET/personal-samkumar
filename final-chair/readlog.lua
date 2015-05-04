fc3c317c03e875f85eebea2b425813dc2c06eaeb
Sam Kumar <samkumar@berkeley.edu>
2015-05-04 12:24:03 -0700

    Updated log printing program

--- final-chair/readlog.lua
+++ final-chair/readlog.lua
@@ -4,9 +4,12 @@ cord.new(function ()
     local log_length = cord.await(storm.n.flash_get_log_size)
     local final_log_index = log_length - 1
     print("=============== START LOG ===============")
-    print("Index", "Time", "Back Heat", "Bottom Heat", "Back Fan", "Bottom Fan", "Temperature", "Humidity", "Occupancy", "Rebooted")
+    print("Index", "Time", "", "Back Heat", "Bottom Heat", "Back Fan", "Bottom Fan", "Temperature", "Humidity", "Occupancy", "Rebooted")
     for i = 0, final_log_index do
         time, backh, bottomh, backf, bottomf, temp, hum, occ, rebooted = cord.await(storm.n.flash_read_log, i)
+        if time == nil then
+            time = "Unknown Time"
+        end
         print(i, time, backh, "", bottomh, "", backf, "", bottomf, "", temp, "", hum, "", occ, "", rebooted)
     end
     print("================ END LOG ================")
